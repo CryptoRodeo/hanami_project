ff23f183d7ae000179b1fbfa05ec07904da7df21
bryan ramos <b.r.tech95@gmail.com>
2020-02-11 15:27:37 -0500

    modeling data entities in Hanami

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -4,4 +4,6 @@
 # Example:
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
 # apps/web/config/routes.rb
+#point to the apps root url to the index action of the home controller.
 root to: 'home#index'
+get '/books', to: 'books#index'
