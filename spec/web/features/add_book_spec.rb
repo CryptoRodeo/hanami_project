906cd74ddfddc317a2e0151ec1635279d5d7d85f
CoffeeCodeCreate <b.r.tech95@gmail.com>
2020-02-12 14:09:38 -0500

    Adding spec for form

---
+++ spec/web/features/add_book_spec.rb
@@ -0,0 +1,23 @@
+# When the user submits the form, we build a new entity, save it, and redirect the user back to the book listing.
+
+require 'features_helper'
+
+RSpec.describe 'Add a book' do
+    after do
+        BookRepository.new.clear
+    end
+
+    it 'can create a new book' do
+        visit '/books/new'
+
+        within 'form#book-form' do
+            fill_in 'Title', with: 'New book'
+            fill_in 'Author', with: 'Some author'
+
+            click_button 'create'
+        end
+        expect(page).to have_current_path('/books')
+        expect(page).to have_content('New book')
+    end
+end
+
