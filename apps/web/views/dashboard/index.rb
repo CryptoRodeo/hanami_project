afd2493b95ff63ae063c97f863e211b82990ef0f
Bryan Ramos <b.r.tech95@gmail.com>
2020-02-22 18:38:31 -0500

    removing vscode text

--- apps/web/views/dashboard/index.rb
+++ apps/web/views/dashboard/index.rb
@@ -3,12 +3,6 @@ module Web
     module Dashboard
       class Index
         include Web::View
-<<<<<<< HEAD
-
-        def title
-          'Admin Dashboard'
-        end
-=======
         require "net/http"
         def title
           'Admin Dashboard'
@@ -18,7 +12,6 @@ module Web
           ip = Net::HTTP.get(URI("https://api.ipify.org"))
           ip
         end
->>>>>>> b9d7b9aace0990a170c19f4e230cba2a29779a00
       end
     end
   end
