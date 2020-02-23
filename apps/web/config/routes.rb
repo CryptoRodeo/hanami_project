dbab2d4469363008284f8e1d2f6d94bdfa6f8644
Bryan Ramos <b.r.tech95@gmail.com>
2020-02-22 19:12:03 -0500

    finally got rid of the vscode garbage

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -6,14 +6,6 @@
 # apps/web/config/routes.rb
 #point to the apps root url to the index action of the home controller.
 =begin
-<<<<<<< HEAD
-         Name Method     Path                           Action
-
-     root GET, HEAD  /                              Web::Controllers::Home::Index
-    books GET, HEAD  /books                         Web::Controllers::Books::Index
- new_book GET, HEAD  /books/new                     Web::Controllers::Books::New
-    books POST       /books                         Web::Controllers::Books::Create
-=======
     Name       Method     Path                           Action
 
     root       GET,HEAD  /                              Web::Controllers::Home::Index
@@ -21,20 +13,13 @@
     new_book   GET,HEAD  /books/new                     Web::Controllers::Books::New
     books      POST      /books                         Web::Controllers::Books::Create
 
->>>>>>> b9d7b9aace0990a170c19f4e230cba2a29779a00
 =end
 
 #index action of the home controller
 root to: 'home#index'
 resources :books, only: [:index, :new, :create]
-<<<<<<< HEAD
-get '/hello', to: ->(env) { [200,{}, ['Hello from hanami']] }
-
-get '/dashboard/', to: 'dashboard#index'
-=======
 get '/hello', to: ->(env) { [200, {}, ['Hello from hanami']] }
 
 get '/dashboard/', to: 'dashboard#index'
 get '/secret/:message', to: 'secret#index'
 
->>>>>>> b9d7b9aace0990a170c19f4e230cba2a29779a00
--- spec/web/features/visit_admin_spec.rb
+++ spec/web/features/visit_admin_spec.rb
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-=======
 require 'features_helper'
 
->>>>>>> b9d7b9aace0990a170c19f4e230cba2a29779a00
 RSpec.describe 'Has title Admin' do
     it "has the title 'Admin'" do
         visit '/dashboard'
