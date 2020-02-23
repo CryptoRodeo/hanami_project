e1b23d1a157c184da263bb392475e2ae21ae5f0f
Bryan Ramos <b.r.tech95@gmail.com>
2020-02-22 19:05:19 -0500

    removing more trash

--- apps/web/controllers/dashboard/index.rb
+++ apps/web/controllers/dashboard/index.rb
@@ -4,13 +4,9 @@ module Web
       class Index
         include Web::Action
 
-<<<<<<< HEAD
-        def call(params)
-=======
 
         def call(params)
           puts "======================================"
->>>>>>> b9d7b9aace0990a170c19f4e230cba2a29779a00
         end
       end
     end
